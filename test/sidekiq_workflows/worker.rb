0b86d79c9e7d20fa9963f42f1b0994ba28dc7bd9
ditjonberisha <ditjonberisha@gmail.com>
2023-03-30 14:40:42 +0200

    fix

--- test/sidekiq_workflows/worker.rb
+++ test/sidekiq_workflows/worker.rb
@@ -7,15 +7,15 @@ module SidekiqWorkflows
     def perform(test_uuid, worker_name)
       raise StandardError if worker_name == 'ErrorWorker'
 
-      Redis.new(url: 'redis://redis:6379').sadd(test_uuid, worker_name)
+      Redis.new(url: ENV.fetch('REDIS_URL', 'redis://redis:6379')).sadd(test_uuid, worker_name)
     end
 
     def on_success(status, options)
-      Redis.new(url: 'redis://redis:6379').sadd(options['workflow_uuid'], 'success')
+      Redis.new(url: ENV.fetch('REDIS_URL', 'redis://redis:6379')).sadd(options['workflow_uuid'], 'success')
     end
 
     def on_death(status, options)
-      Redis.new(url: 'redis://redis:6379').sadd(options['workflow_uuid'], 'death')
+      Redis.new(url: ENV.fetch('REDIS_URL', 'redis://redis:6379')).sadd(options['workflow_uuid'], 'death')
     end
   end
 end
