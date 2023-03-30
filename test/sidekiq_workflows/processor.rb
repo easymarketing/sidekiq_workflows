5783123ea6094f98d0b4fad8ede96878232daab2
ditjonberisha <ditjonberisha@gmail.com>
2023-03-30 11:57:11 +0200

    fix

--- test/sidekiq_workflows/processor.rb
+++ test/sidekiq_workflows/processor.rb
@@ -3,9 +3,9 @@ require_relative './worker'
 require_relative '../../lib/sidekiq_workflows'
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { url: 'redis://redis:6379' }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { url: 'redis://redis:6379' }
 end
